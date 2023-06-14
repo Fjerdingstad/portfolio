17615ef04dd474820d250687d8e7e67402e8efd4
Fjerdingstad <32462102+Fjerdingstad@users.noreply.github.com>
2023-06-14 12:57:03 +0200

    Add files via upload

---
+++ Angular + C#/address book/visual code project/test-project2-backend/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test_project2_backend.Models
+{
+    public class Contact
+    {
+        [Key]
+        public Guid ContactId { get; set; }
+
+        public string ContactFirstName { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactAddress1 { get; set; }
+        public string ContactPostalCode1 { get; set; }
+        public string ContactAddress2 { get; set; }
+        public string ContactPostalCode2 { get; set; }
+        public string ContactCity1 { get; set; }
+
+        public string ContactCity2 { get; set; }
+
+    }
+}
---
+++ Angular + C#/address book/visual code project/test-project2-backend/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using test_project2_backend.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Inject DbContext
+builder.Services.AddDbContext<ContactDBContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("ContactDBConnectionString")));
+
+builder.Services.AddCors((setup) =>
+{
+    setup.AddPolicy("default", (options) =>
+    {
+        options.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin(); //allowing any UI to talk to API
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("default");  //use the policy
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
